df1f774a33e9b924465bdb82e5df129e9a2abca2
Paulo <pauloh16.pdr@gmail.com>
2021-01-24 15:40:13 -0300

    Adicionar dependencias do front-end

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// Importando frameworks de css
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+// Componentes da interface.
 import App from "./App";
 
 ReactDOM.render(
