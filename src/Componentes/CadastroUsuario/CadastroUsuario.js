918e811aaca99a31af05a90be597039fb3107d89
Paulo <pauloh16.pdr@gmail.com>
2021-01-25 12:25:23 -0300

    Criar logica inicial no componente CadastroUsuario

--- src/Componentes/CadastroUsuario/CadastroUsuario.js
+++ src/Componentes/CadastroUsuario/CadastroUsuario.js
@@ -2,7 +2,50 @@ import React from "react";
 // Importando componentes da interface.
 import Main from "../Main/Main.js";
 
+const baseURL = "http://localhost:3001/users";
+
+const initialState = {
+    lista: [],
+    user: {
+        name: "",
+        email: ""
+    }
+};
+
 const CadastroUsuario = () => {
+    const [lista, setLista] = React.useState(initialState.lista);
+    const [user, setUser] = React.useState(initialState.user);
+
+    const limpar = () => setUser(initialState.user);
+
+    const atualizarCampos = ({ target }) => setUser((user) => ({
+        ...user,
+        [target.name]: target.value
+    }));
+
+    const salvar = async () => {
+        const method = (user?.id) ? "PUT" : "POST";
+        const url = (user?.id) ? `${baseURL}/${user.id}` : baseURL;
+
+        const res = await fetch(url, {
+            method,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        });
+
+        const json = await res.json();
+
+        setLista((lista) => {
+            const listaNova = lista.filter((item) => item.id !== json.id );
+            listaNova.unshift(json);
+
+            limpar();
+            return listaNova;
+        });
+    };
+
     return (
         <Main
             icon="users"
