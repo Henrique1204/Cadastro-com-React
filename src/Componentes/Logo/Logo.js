8e25e288f31112af3923c82888947f82d7f06806
Paulo <pauloh16.pdr@gmail.com>
2021-01-25 09:07:40 -0300

    Implementar componente Logo

--- src/Componentes/Logo/Logo.js
+++ src/Componentes/Logo/Logo.js
@@ -1,11 +1,15 @@
 import React from "react";
 // Importando estilo do componente.
 import estilos from "./Logo.module.css";
+// Importando Assets.
+import logoImg from "../../Assets/img/logo.png";
 
 const Logo = () => {
     return (
         <aside className={`logo ${estilos.Logo}`}>
-            Logo
+            <a href="/">
+                <img src={logoImg} alt="Logo"/>
+            </a>
         </aside>
     );
 };
